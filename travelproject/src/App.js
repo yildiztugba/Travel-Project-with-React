c458d8944ed340670f8cee70e5bebef9464e07a3
yildiztugba <okra.yildiz@gmail.com>
2023-02-01 14:18:30 +0100

    import components

--- travelproject/src/App.js
+++ travelproject/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar"
+import Card from "./components/Card"
 
 function App() {
   return (
-<h1>tugba</h1>
+    <Navbar />
+    <Card />
+
   )
 }
 
