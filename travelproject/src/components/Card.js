bb3662f643862867b4058c245f7fdb6ee01dba17
yildiztugba <okra.yildiz@gmail.com>
2023-02-01 14:51:08 +0100

    structure of the card

--- travelproject/src/components/Card.js
+++ travelproject/src/components/Card.js
@@ -3,9 +3,15 @@ import react from "react"
 export default function Card(props){
 console.log(props)
 return(
-<>
-<h1>card</h1>
-</>
+<div className="Session">
+<img src=""/>
+<img src=""/>
+<h4></h4>
+<h5></h5>
+<h2></h2>
+<h3></h3>
+<p></p>
+</div>
 )
 
 }
