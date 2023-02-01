edb6655a8324c87f59d0e97fcb9fc40b739db2e3
yildiztugba <okra.yildiz@gmail.com>
2023-02-01 14:20:17 +0100

    render with H1

---
+++ travelproject/src/components/Card.js
@@ -0,0 +1,8 @@
+import react from "react"
+
+export default function Card(){
+return(
+    <h1>card</h1>
+)
+
+}
---
+++ travelproject/src/components/Navbar.js
@@ -0,0 +1,8 @@
+import react from "react"
+
+export default function Navbar(){
+return(
+    <h1>Navbar</h1>
+)
+    
+}
