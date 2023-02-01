b4336d9f917403003042416966b1af8111b8a6b1
yildiztugba <okra.yildiz@gmail.com>
2023-02-01 16:03:32 +0100

    table, cell,row

--- travelproject/src/components/Card.js
+++ travelproject/src/components/Card.js
@@ -3,14 +3,22 @@ import react from "react"
 export default function Card(props){
 console.log(props)
 return(
-<div className="session">
-<img src={props.item.imageUrl} alt="location" className="image"/>
-<img src=""/>
-<h4>{props.item.location}</h4>
-<h5>View on Google Maps</h5>
-<h2>{props.item.title}</h2>
-<h3>{props.item.startDate} - {props.item.endDate}</h3>
-<p>{props.item.description}</p>
+<div className="session table">
+    <div className="row">
+        <div className="cell">
+            <img src={props.item.imageUrl} alt="location" className="image"/>
+        </div>
+        <div className="cell">
+            <div><img src=""/>
+            <div className="sameLine">
+            <h4>{props.item.location}</h4>
+            <h5>View on Google Maps</h5></div>
+            <h2>{props.item.title}</h2>
+            <h3>{props.item.startDate} - {props.item.endDate}</h3>
+            <p>{props.item.description}</p>
+            </div>
+        </div>
+    </div>
 </div>
 )
 
